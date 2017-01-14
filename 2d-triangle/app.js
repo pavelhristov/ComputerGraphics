0bee4ee3873507f48e1354aeda06f3ee8b45d25c
pavelhristov <pavel.hristov_@abv.bg>
2017-01-15 01:50:26 +0200

    following some tutorials

---
+++ 2d-triangle/app.js
@@ -0,0 +1,120 @@
+const vertexShaderText = [
+    'precision mediump float;',
+    '',
+    'attribute vec2 vertPosition;',
+    'attribute vec3 vertColor;',
+    'varying vec3 fragColor;',
+    '',
+    'void main()',
+    '{',
+    '   fragColor = vertColor;',
+    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
+    '}'
+].join('\n');
+
+const fragmentShaderText = [
+    'precision mediump float;',
+    '',
+    'varying vec3 fragColor;',
+    '',
+    'void main()',
+    '{',
+    '   gl_FragColor = vec4(fragColor, 1.0);',
+    '}'
+].join('\n');
+
+
+var initGl = function() {
+    // Initiliazing WebGL
+    const canvas = document.getElementById('surface');
+    let gl = initWebGL(canvas);
+
+    // Setting clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Create Shaders
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.error('EROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error('EROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    // Create buffer
+    const triangleVertices = [
+        // x, y,  R,  G,   B
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 1.0,
+        0.0, 0.5, 1.0, 0.0, 0.0
+    ];
+
+    let triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    let colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        positionAttribLocation, // Attribute location
+        2, // Number of elements per atribute
+        gl.FLOAT, // Type of elements
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        0 // Offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.vertexAttribPointer(
+        colorAttribLocation, // Attribute location
+        3, // Number of elements per atribute
+        gl.FLOAT, // Type of elements
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colorAttribLocation);
+
+    // Main render loop
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+};
+
+function initWebGL(canvas) {
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    let gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+    // If we don't have a GL context, give up now
+    if (!gl) {
+        alert("Unable to initialize WebGL. Your browser may not support it.");
+    }
+
+    return gl;
+}
