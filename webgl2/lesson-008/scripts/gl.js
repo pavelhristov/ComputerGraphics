0c18dddf31f212eafa6cf2408de569d3731f16dc
pavelhristov <pavel.hristov_@abv.bg>
2017-09-17 16:33:51 +0300

    lesson 8

---
+++ webgl2/lesson-008/scripts/gl.js
@@ -0,0 +1,159 @@
+// Global constants
+const ATTR_POSITION_NAME = 'a_position';
+const ATTR_POSITION_LOCATION = 0;
+const ATTR_NORMAL_NAME = 'a_norm';
+const ATTR_NORMAL_LOCATION = 1;
+const ATTR_UV_NAME = 'a_uv';
+const ATTR_UV_LOCATION = 2;
+
+
+// Custom GL Context Object
+function GLInstance(canvasId) {
+
+    // Init GL
+    let canvas = document.getElementById(canvasId);
+    const gl = canvas.getContext('webgl2');
+
+    if (!gl) {
+        console.error("WebGL2 is not supported by this browser!");
+        return null;
+    }
+
+    //Setup custom properties
+    gl.mMeshCache = []; // Cashe all the mesh structs, easy to unload buffers if they are all exist in one place.
+    gl.mTextureCache = [];
+
+
+    // Setup GL. Default configurations
+    gl.cullFace(gl.BACK); // dont render back
+    gl.frontFace(gl.CCW); // counter clock wise rendering
+    gl.enable(gl.DEPTH_TEST); // Fragments closer to camera overrides further ones
+    gl.enable(gl.CULL_FACE); // Cull back face, so only show triangles that are created clockwise
+    gl.depthFunc(gl.LEQUAL); // Near things obscure further things
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA); // Setup default alpha blending
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // Methods
+
+    // Reset the canvas with our set background color.
+    gl.fClear = function () {
+        // Clearing the color and the depth buffer.
+        this.clear(this.COLOR_BUFFER_BIT | this.DEPTH_BUFFER_BIT);
+        return this;
+    }
+
+    // Create and fill our Array buffer.
+    gl.fCreateArrayBuffer = function (floatArray, isStatic) {
+        if (isStatic === undefined) isStatic = true;
+
+        let buffer = this.createBuffer();
+        this.bindBuffer(this.ARRAY_BUFFER, buffer);
+        this.bufferData(this.ARRAY_BUFFER, floatArray, (isStatic) ? this.STATIC_DRAW : this.DYNAMIC_DRAW);
+        this.bindBuffer(this.ARRAY_BUFFER, null);
+
+        return buffer;
+    }
+
+    // Turns array into GL buffers, then setup a VAO that will predefine the buffers to strandard shader attributes.
+    gl.fCreateMeshVAO = function (name, aryInd, aryVert, aryNorm, aryUV) {
+        let rtn = { drawMode: this.TRIANGLES };
+
+        // Create and bind VAO
+        rtn.vao = this.createVertexArray();
+        this.bindVertexArray(rtn.vao); //Bind it so all the calls to vertexAttribPointer/enableVertexAttribArray is saved to the vao.
+
+        // Setup vertices
+        if (aryVert !== undefined && aryVert != null) {
+            rtn.bufVertices = this.createBuffer(); // Create buffer
+            rtn.vertexComponentLen = 3; // How many floats make up a vertex
+            rtn.vertexCount = aryVert.length / rtn.vertexComponentLen; // How many vertices in the array
+
+            this.bindBuffer(this.ARRAY_BUFFER, rtn.bufVertices);
+            this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryVert), this.STATIC_DRAW); // Push array
+            this.enableVertexAttribArray(ATTR_POSITION_LOCATION); // Enable attribute location
+            this.vertexAttribPointer(ATTR_POSITION_LOCATION, 3, this.FLOAT, false, 0, 0); // Put buffer at location of the vao
+        }
+
+        // Setup normals
+        if (aryNorm !== undefined && aryNorm != null) {
+            rtn.bufNormals = this.createBuffer();
+            this.bindBuffer(this.ARRAY_BUFFER, rtn.bufNormals);
+            this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryNorm), this.STATIC_DRAW);
+            this.enableVertexAttribArray(ATTR_NORMAL_LOCATION);
+            this.vertexAttribPointer(ATTR_NORMAL_LOCATION, 3, this.FLOAT, false, 0, 0);
+        }
+
+        // Setup UV
+        if (aryUV !== undefined && aryUV != null) {
+            rtn.bufUV = this.createBuffer();
+            this.bindBuffer(this.ARRAY_BUFFER, rtn.bufUV);
+            this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryUV), this.STATIC_DRAW);
+            this.enableVertexAttribArray(ATTR_UV_LOCATION);
+            this.vertexAttribPointer(ATTR_UV_LOCATION, 2, this.FLOAT, false, 0, 0); // UV only has two floats per component
+        }
+
+        // Setup Index
+        if (aryInd !== undefined && aryInd != null) {
+			rtn.bufIndex = this.createBuffer();
+			rtn.indexCount = aryInd.length;
+			this.bindBuffer(this.ELEMENT_ARRAY_BUFFER, rtn.bufIndex);
+			this.bufferData(this.ELEMENT_ARRAY_BUFFER, new Uint16Array(aryInd), this.STATIC_DRAW);
+			// this.bindBuffer(this.ELEMENT_ARRAY_BUFFER, null);
+		}
+
+        // Clean up
+        this.bindVertexArray(null); //Unbind the VAO, very Important. Always unbind when you are done using it.
+        this.bindBuffer(this.ARRAY_BUFFER, null); // Unbind any buffers that might be set
+
+        if (aryInd) {
+            this.bindBuffer(this.ELEMENT_ARRAY_BUFFER, null);
+        }
+
+        this.mMeshCache[name] = rtn;
+        return rtn;
+    }
+
+    gl.fLoadTexture = function(name, img, doYFlip){
+        let tex =  this.createTexture();
+        if(doYFlip){
+            this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, true); // Flip texture by the Y position
+        }
+
+        this.bindTexture(this.TEXTURE_2D, tex); // Setup texture buffer
+        this.texImage2D(this.TEXTURE_2D, 0, this.RGBA, this.RGBA, this.UNSIGNED_BYTE, img); // Push image to GPU
+
+        this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MAG_FILTER, this.LINEAR); // Setup up scaling
+        this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MIN_FILTER, this.LINEAR_MIPMAP_NEAREST); // Setup down scaling
+        this.generateMipmap(this.TEXTURE_2D); // Precalc different sizes of texture for better quality rendering
+
+        this.bindTexture(this.TEXTURE_2D, null); // Unbind
+        this.mTextureCache[name] = tex; // Save ID for later unloading
+
+        if(doYFlip === true){
+            this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, false); // Stop flipping textures
+        }
+
+        return tex;
+    }
+
+    // Setters and Getters
+
+    // Set the size of the canvas and the rendering view port
+    gl.fSetSize = function (w, h) {
+        // set the size of the canvas, 3 ways needed to work properly on chrome
+        this.canvas.style.width = `${w}px`;
+        this.canvas.style.height = `${h}px`;
+        this.canvas.width = w;
+        this.canvas.height = h;
+
+        // when updating the canvas size, must reset the viewport of the canvas to change webgl render resolution
+        this.viewport(0, 0, w, h);
+        return this;
+    }
+
+    gl.fFitScreen = function (wp, hp) {
+        return this.fSetSize(window.innerWidth * (wp || 1), window.innerHeight * (hp || 1));
+    }
+
+    return gl;
+}
---
+++ webgl2/lesson-008/scripts/primitives.js
@@ -0,0 +1,171 @@
+let Primitives = {};
+
+Primitives.Quad = class {
+    static createModal(gl) {
+        return new Modal(Primitives.Quad.createMesh(gl));
+    }
+
+    static createMesh(gl) {
+        let aVert = [-0.5, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0, 0.5, 0.5, 0];
+        let aUV = [0, 0, 0, 1, 1, 1, 1, 0];
+        let aIndex = [0, 1, 2, 2, 3, 0];
+        let mesh = gl.fCreateMeshVAO('Quad', aIndex, aVert, null, aUV);
+        mesh.noCulling = true;
+        mesh.doBlending = true;
+        return mesh;
+    }
+}
+
+Primitives.MultiQuad = class {
+    static createModal(gl) {
+        return new Modal(Primitives.MultiQuad.createMesh(gl));
+    }
+
+    static createMesh(gl) {
+        let aIndex = [];
+        let aUV = [];
+        let aVert = [];
+
+        for (let i = 0; i < 10; i++) {
+            // Calculate a random size, y rotation and position for the quad
+            let size = 0.2 + (0.8 * Math.random());     // Random Quad Size in the range of 0.2 to 1.0
+            let half = size * 0.5;                      // Half of the size, this is the radius for rotation
+            let angle = Math.PI * 2 * Math.random();    // Random angle between 0 and 360 degrees in radians
+            let dx = half * Math.cos(angle);            // Calc the x distance, used as an offset for the random position
+            let dy = half * Math.sin(angle);            // Calc the y distance, for same offset but used in z
+            let x = -2.5 + (Math.random() * 5);         // Random betwean 2.5 and -2.5
+            let y = -2.5 + (Math.random() * 5);
+            let z = 2.5 - (Math.random() * 5);
+            let p = i * 4;                              // Index of first vertex of a squad
+
+            // Build the 4 points of the quad
+            aVert.push(x - dx, y + half, z - dy);       // TOP LEFT
+            aVert.push(x - dx, y - half, z - dy);       // BOTTOM LEFT
+            aVert.push(x + dx, y - half, z + dy);       // BOTTOM RIGHT
+            aVert.push(x + dx, y + half, z + dy);       // TOP RIGHT
+
+            aUV.push(0, 0, 0, 1, 1, 1, 1, 0);           // Quad's UV
+            aIndex.push(p, p + 1, p + 2, p + 2, p + 3, p);     // Quad's Index
+        }
+
+        let mesh = gl.fCreateMeshVAO('MultiQuad', aIndex, aVert, null, aUV);
+        mesh.noCulling = true;
+        mesh.doBlending = true;
+        return mesh;
+    }
+}
+
+Primitives.GridAxis = class {
+    static createModal(gl, inculdeAxis) {
+        return new Modal(Primitives.GridAxis.createMesh(gl, inculdeAxis));
+    }
+    static createMesh(gl, includeAxis) {
+        let verts = [];
+        let size = 2; // W/H of the outer box of the grid, from origin we can only go 1 unit in each direction so from left to right is 2 units max
+        let div = 10.0; // How to divide up the grid
+        let step = size / div; // Steps between each line, just a number we increment by for each line in the grid
+        let half = size / 2; // From origin the starting position is half the size.
+
+        let p; // Temp variable for position value
+        for (let i = 0; i <= div; i++) {
+            // Vertical line
+            p = -half + (i * step);
+            verts.push(p); //x1
+            verts.push(0) //y1
+            verts.push(half); //z1
+            verts.push(0); //c1
+
+            verts.push(p); //x2
+            verts.push(0) //y2
+            verts.push(-half); //z2;
+            verts.push(0); //c2
+
+            //Horizontal line
+            p = half - (i * step);
+            verts.push(-half); //x1
+            verts.push(0) //y1
+            verts.push(p); //z1
+            verts.push(0); //c1
+
+            verts.push(half); //x2
+            verts.push(0) //y2
+            verts.push(p); //z2;
+            verts.push(0); //c2
+        }
+
+        if (includeAxis) {
+            // x axis
+            verts.push(-1.1); //x1
+            verts.push(0) //y1
+            verts.push(0); //z1
+            verts.push(1); //c1
+
+            verts.push(1.1); //x2
+            verts.push(0) //y2
+            verts.push(0); //z2
+            verts.push(1); //c2
+
+            // y axis
+            verts.push(0); //x1
+            verts.push(-1.1) //y1
+            verts.push(0); //z1
+            verts.push(2); //c1
+
+            verts.push(0); //x2
+            verts.push(1.1) //y2
+            verts.push(0); //z2
+            verts.push(2); //c2
+
+            // x axis
+            verts.push(0); //x1
+            verts.push(0) //y1
+            verts.push(-1.1); //z1
+            verts.push(3); //c1
+
+            verts.push(0); //x2
+            verts.push(0) //y2
+            verts.push(1.1); //z2
+            verts.push(3); //c2
+        }
+
+        let attrColorLocation = 4;
+        let strideLength;
+        let mesh = { drawMode: gl.LINES, vao: gl.createVertexArray() };
+
+        // Do some math
+        mesh.vertexComponentLength = 4;
+        mesh.vertexCount = verts.length / mesh.vertexComponentLength;
+        strideLength = Float32Array.BYTES_PER_ELEMENT * mesh.vertexComponentLength; // Stride Length is the Vertex Size for the buffer in Bytes
+
+        // Setup our Buffer
+        mesh.bufVertices = gl.createBuffer();
+        gl.bindVertexArray(mesh.vao);
+        gl.bindBuffer(gl.ARRAY_BUFFER, mesh.bufVertices);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+        gl.enableVertexAttribArray(ATTR_POSITION_LOCATION);
+        gl.enableVertexAttribArray(attrColorLocation);
+
+        gl.vertexAttribPointer(
+            ATTR_POSITION_LOCATION, // Attribute Location
+            3, // How big is the vector by number count
+            gl.FLOAT, // What type of number we passing in
+            false, // Does it need to be normalized
+            strideLength, // How big is a vertex chunk of data
+            0); // OffSet by how much
+
+
+        gl.vertexAttribPointer(
+            attrColorLocation, // new shader has 'in float a_color' as second attrib
+            1, // This attrib is a single float
+            gl.FLOAT,
+            false,
+            strideLength, // Each vertex chunk is 4 floats long
+            Float32Array.BYTES_PER_ELEMENT * 3); // Skip first 3 floats
+
+        // Cleanup and Finalize
+        gl.bindVertexArray(null);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.mMeshCache['grid'] = mesh;
+        return mesh;
+    }
+}
